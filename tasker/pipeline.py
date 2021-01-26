4335e2632c1ea9e90a6ffde5c1a4541b8e352360
Karlisson Bezerra <macedobz@gmail.com>
2021-01-26 01:02:15 -0300

    Add task names

--- tasker/pipeline.py
+++ tasker/pipeline.py
@@ -1,11 +1,11 @@
 
 class Task:
-    def __init__(self, name='anonym', func=lambda:None):
+    def __init__(self, name, function):
         self.name = name
-        self.func = func
+        self.function = function
 
     def run(self):
-        pass
+        print(f'Ran {self.name}')
 
     def __str__(self):
         return f'task="{self.name}"'
@@ -15,10 +15,10 @@ class Tasker:
     def __init__(self):
         self._tasks = []
 
-    def task(self, name='', base=Task):
+    def task(self, name='anonym', base=Task):
         def wrapped(original_method):
-            def decorated_task(method_self, *args, **kwargs):
-                return original_method(method_self, *args, **kwargs)
+            def decorated_task(method_self):
+                return original_method(method_self)
             _task = base(name, decorated_task)
             self._tasks.append(_task)
             return decorated_task
@@ -26,8 +26,7 @@ class Tasker:
 
     def run(self):
         for task in self._tasks:
-            print(task)
-            # result = task.run()
+            result = task.run()
             # if not result.valid:
             #     return result
 
